4277db00c5848a7252144cf66b69ff14bbb22fb5
Jellybeanci <gokselkucuksahin@gmail.com>
2020-12-12 21:15:06 +0300

    Hadouken💨

--- docs/assets/scripts/controller.js
+++ docs/assets/scripts/controller.js
@@ -1,1 +1,31 @@
-console.log("hello?");
+new Vue({
+  el: "#navbar_app",
+  data: {
+    isMobile: false,
+    isOpen: false,
+  },
+  created() {
+    window.addEventListener("resize", this.myEventHandler);
+  },
+  destroyed() {
+    window.removeEventListener("resize", this.myEventHandler);
+  },
+  mounted() {
+    this.toggle(window.innerWidth);
+  },
+  methods: {
+    hamburgerClick() {
+      this.isOpen = !this.isOpen;
+    },
+    toggle(width) {
+      this.isMobile = width < 992;
+      if (this.isOpen === true && this.isMobile === false) {
+        this.isOpen = false;
+      }
+    },
+    myEventHandler(e) {
+      //console.log(e.target.innerWidth);
+      this.toggle(e.target.innerWidth);
+    },
+  },
+});

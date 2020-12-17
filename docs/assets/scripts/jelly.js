2d8e7e653e8d33819bc7a2b98ce7fce5504f34a7
Jellybeanci <gokselkucuksahin@gmail.com>
2020-12-18 01:11:22 +0300

    Left side ready... I guess :p

---
+++ docs/assets/scripts/jelly.js
@@ -0,0 +1,16 @@
+new Vue({
+  el: "#app",
+  data: {
+    aboutMe:
+      "Ben Göksel KÜÇÜKŞAHİN, Erciyes Üniversitesi\
+     Bilgisayar Mühendisliği 4. öğrencisiyim. 1998'de \
+    Kayseri Kocasinan'da doğdum. İlk ve orta okulu \
+    Osman Düşüngel Orta Okulunda; Liseyi Melikgaz\
+     Mustafa Eminoğlu Lisesinde okudum.",
+     details:  "Küçük yaştan itibaren teknoloji \
+     ve bilgisayarlar karşı ilgili olmamdan dolayı \
+     Bilgisayar Mühendisi olmaya karar verdim. Hobilerim; Akrilik \
+     tablo yapmak🎨 ve video oyunları🕹. Sıkı Star Wars🌑 ve IronMaiden \
+     hayranıyımdır🤘. "
+  },
+});

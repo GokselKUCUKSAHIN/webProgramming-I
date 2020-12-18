5f39c77b3020cd9a8f81be5150cd81b2e1f1fb45
Jellybeanci <gokselkucuksahin@gmail.com>
2020-12-18 14:32:45 +0300

    Listeler v-for ile yazdırıldı

--- docs/assets/scripts/controller.js
+++ docs/assets/scripts/controller.js
@@ -29,3 +29,73 @@ new Vue({
     },
   },
 });
+
+new Vue({
+  el: "#card_app",
+  data: {
+    imgs: [
+      "assets/res/site/img/0victoria.jpg",
+      "assets/res/site/img/0peace.jpg",
+      "assets/res/site/img/0aviator.jpg",
+      "assets/res/site/img/0hendrix.jpg",
+      "assets/res/site/img/0pickup.jpg",
+      "assets/res/site/img/0episode5.jpg",
+      "assets/res/site/img/0adam.jpg",
+      "assets/res/site/img/0norman.jpg",
+      "assets/res/site/img/0slash.jpg",
+      "assets/res/site/img/0dave.jpg",
+      "assets/res/site/img/0leger.jpg",
+      "assets/res/site/img/0riff_lord.jpg",
+      "assets/res/site/img/0guitar.jpg",
+      "assets/res/site/img/0virtual.jpg",
+      "assets/res/site/img/0string.jpg",
+      "assets/res/site/img/0g3.jpg",
+    ],
+  },
+});
+
+new Vue({
+  el: "#footer_app",
+  data: {
+    gmodels: [
+      "Les Paul Models",
+      "SG Models",
+      "ES-Style Models",
+      "Basses",
+      "Designer Models",
+      "Gibson Custom Shop",
+      "Super Jumbo Models",
+      "Square Shoulder Models",
+      "Round Shoulder Models",
+      "Small Body Models",
+      "Songwriter Models",
+      "Gibson Gear",
+      "Gibson Hardware",
+      "Gibson Lifestyle & Apparel",
+    ],
+    ourcomp: [
+      "About Gibson Brands",
+      "Careers",
+      "Contact Us",
+      "News",
+      "Gibson TV",
+      "Gibson Gives",
+      "Gibson Generation Group",
+    ],
+    supp: [
+      "Customer Service",
+      "Virtual Guitar Tech",
+      "Warranty Registration & Info",
+      "Repair and Restoration",
+      "Report Counterfeits",
+      "Serial Number FAQ",
+      "Store Policies & FAQ",
+      "Gibson Guitar Specs (2015-2019)",
+    ],
+    dealer: [
+      "Find a Gibson Dealer",
+      "Online Dealers",
+      "Dealer Resource Center",
+    ],
+  },
+});
